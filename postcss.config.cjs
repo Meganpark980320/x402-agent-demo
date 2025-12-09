9ce9a30a988a9ffdb030126194963abfb8f7c6af
Meganpark <meganpark980320@gmail.com>
2025-12-09 19:13:45 +0900

    Install @tailwindcss/postcss to resolve package move error

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    // 👇 여기에 추가합니다!
+    '@tailwindcss/postcss': {}, // Tailwind CSS PostCSS 플러그인
+    tailwindcss: {}, // 기존 설정 유지 (혹시 모를 호환성 위해)
     autoprefixer: {},
   },
 };
